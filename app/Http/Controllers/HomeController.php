7b341aed222ed01cfbb959b9c8eb3bb645d4de38
hillghost86 <54775494+hillghost86@users.noreply.github.com>
2020-03-20 02:43:25 +0800

    Add files via upload

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,14 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Category;
-
 class HomeController extends Controller
-{
+{ 
+    /**
+     * 新增排序功能
+     */
     public function index()
     {
         $categories = Category::with(['children' => function ($query) {
-            $query->orderBy('order');
-        }, 'sites'])
+            $query->orderBy('order'); //分类排序 数字越小越靠前
+        }, 'sites'=> function($query){
+            $query->orderBy('order','desc');   //网址排序,逆序,数字越大越靠前
+        }])
             ->withCount('children')
             ->orderBy('order')
             ->get();
